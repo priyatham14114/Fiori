ca9ccc66cf8fa08fdc7c9afb633745728d18f748
priyatham subhash pydikondala <priyathama.subhash@artihcus.com>
2024-04-22 07:34:38 +0000

    Element Binding

--- webapp/controller/Myview.controller.js
+++ webapp/controller/Myview.controller.js
@@ -11,37 +11,11 @@ sap.ui.define([
         return Controller.extend("appui5.controller.Myview", {
 
             onInit: function(eve){
-                    // debugger;
-            //    const person = {
-            //         Student: {
-            //             name: "Subhash",
-            //             age: "35"
-            //         },
-            //         address: {
-            //             homeTown: "kakinada",
-            //             state: "AndhraPradesh",
-            //             country: "India"
-            //         },
-            //     }
-            //    const person2 = {
-            //         Student: {
-            //             name: "priyatham",
-            //             age: "30"
-            //         },
-            //         address: {
-            //             homeTown: "kkd",
-            //             state: "AP",
-            //             country: "Ind"
-            //         },
-            //     }
-            //    const oMyJson = new Jsonmodel(person);
-            //    const oMyJson2 = new Jsonmodel(person2)
-            //    this.getView().setModel(oMyJson);    // last change give another model
-            //    this.getView().setModel(oMyJson2,"secondModel");   
-            //    console.log(oMyJson);
-            //    console.log(oMyJson2);
 
-
+            },
+            onSubmitData: function(){
+               const Actors = this.getView().getModel().getProperty("/Actors/1")
+               console.log(Actors);
             }
 
         });
