ddf2e0022e0ad5e51ec76047d75fcfb695c84636
priyatham subhash pydikondala <priyathama.subhash@artihcus.com>
2024-04-22 04:33:11 +0000

    Aggrigation binding

--- webapp/controller/Myview.controller.js
+++ webapp/controller/Myview.controller.js
@@ -12,32 +12,32 @@ sap.ui.define([
 
             onInit: function(eve){
                     // debugger;
-               const person = {
-                    Student: {
-                        name: "Subhash",
-                        age: "35"
-                    },
-                    address: {
-                        homeTown: "kakinada",
-                        state: "AndhraPradesh",
-                        country: "India"
-                    },
-                }
-               const person2 = {
-                    Student: {
-                        name: "priyatham",
-                        age: "30"
-                    },
-                    address: {
-                        homeTown: "kkd",
-                        state: "AP",
-                        country: "Ind"
-                    },
-                }
-               const oMyJson = new Jsonmodel(person);
-               const oMyJson2 = new Jsonmodel(person2)
-               this.getView().setModel(oMyJson);    // last change give another model
-               this.getView().setModel(oMyJson2,"secondModel");   
+            //    const person = {
+            //         Student: {
+            //             name: "Subhash",
+            //             age: "35"
+            //         },
+            //         address: {
+            //             homeTown: "kakinada",
+            //             state: "AndhraPradesh",
+            //             country: "India"
+            //         },
+            //     }
+            //    const person2 = {
+            //         Student: {
+            //             name: "priyatham",
+            //             age: "30"
+            //         },
+            //         address: {
+            //             homeTown: "kkd",
+            //             state: "AP",
+            //             country: "Ind"
+            //         },
+            //     }
+            //    const oMyJson = new Jsonmodel(person);
+            //    const oMyJson2 = new Jsonmodel(person2)
+            //    this.getView().setModel(oMyJson);    // last change give another model
+            //    this.getView().setModel(oMyJson2,"secondModel");   
             //    console.log(oMyJson);
             //    console.log(oMyJson2);
 
